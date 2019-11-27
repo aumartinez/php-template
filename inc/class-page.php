353f0ba1983b96d00f5c15319236d74ba035ad79
Augusto Martinez <acm_asd@hotmail.com>
2019-11-27 16:41:39 -0600

    Update class-page.php

--- inc/class-page.php
+++ inc/class-page.php
@@ -4,6 +4,15 @@ class Page {
   public $type = "default";
   public $title = "The web site";
   public $titleExtra = "";
+  
+  public function getHeader() {
+    $html = "";
+    $html .= $this->getDocType();
+    $html .= $this->getHtmlOpen();
+    $html .= $this->getHead();
+    
+    return $html
+  }//End getHeader
 }
 
 ?>
