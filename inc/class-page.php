a75b6476c43f27d118623f9bf815df8fa8cff2b7
Augusto Martinez <acm_asd@hotmail.com>
2019-11-27 17:03:07 -0600

    Update class-page.php

--- inc/class-page.php
+++ inc/class-page.php
@@ -21,7 +21,34 @@ class Page {
     }
     
     return $dtd;
-  }
+  }//End getDocType
+  
+  protected function getHtmlOpen($lang = "en-us") {
+    if ($lang == "en-us") {
+      $htmlopen = "<html lang=\"en\">";
+      $htmlopen .= "\n";
+    }
+    
+    return $htmlopen;
+  }//End getHtmlOpen
+  
+  protected function getHead() {
+    $head = "";
+    $head .= file_get_contents("template/page-head.php");
+    
+    if ($this->titleExtra != "") {
+      $title = $this->titleExtra . "|" . $this->title;
+    }
+    else {
+      $tile = $this->title;
+    }
+    
+    $head .= "<title>" . $title . "</title>\n";
+    $head .= "</head>";
+    $head .= "\n";
+    
+    return $head;    
+  }//End getHead
 }
 
 ?>
