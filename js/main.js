fbe998d65bbfd96c1baa09895d1c0cb579e0f051
Augusto Martinez <acm_asd@hotmail.com>
2019-11-28 17:31:37 -0600

    updates

--- js/main.js
+++ js/main.js
@@ -4,9 +4,52 @@ window.addEventListener("load", run, false);
 
 function run() {
   let btn = document.getElementById("navbar-toggle");
+  let navbar = document.getElementById("navbar");
   
   btn.addEventListener("click", toggleClass, false);
   btn.addEventListener("click", changeSign, false);
+  navbar.addEventListener("active", collapse, false);
+}
+
+function collapse(evt){
+  let elem = evt.currentTarget;  
+    
+  let arr = elem.className.split(" ");
+  let ind = arr.indexOf("active");
+  
+  let inc = 10;
+  
+  if (ind >= 0) {
+    let min = 0;
+    let max = elem.offsetHeight;
+    
+    let collapsing = setInterval(function(){
+      elem.style.height = min + "px";
+      min = min + inc;
+          
+      if (elem.offsetHeight >= max) {      
+          elem.style.height = max + "px";          
+          clearInterval(collapsing);
+        }
+      }, 10);
+  }
+  else {
+    elem.style.display = "block";
+    let max = elem.offsetHeight;
+        
+    let collapsing = setInterval(function(){
+      elem.style.height = max + "px";
+      max = max - inc;
+          
+      if (elem.offsetHeight <= inc) {      
+          elem.style.height = 0 + "px";
+          elem.style.height = "";
+          elem.style.display = "";          
+          clearInterval(collapsing);
+        }
+      }, 10);
+  }
+  
 }
 
 function changeSign(evt) {
@@ -19,7 +62,7 @@ function changeSign(evt) {
   }
 }
 
-function toggleClass(evt) {  
+function toggleClass(evt) {
   let elem = evt.currentTarget;
   let myClass = elem.getAttribute("data-toggle");
   
@@ -30,7 +73,9 @@ function toggleClass(evt) {
     if (elems.length > 0) {
       for (let i = 0; i < elems.length; i++) {
         if (elems[i].classList) {
-          elems[i].classList.toggle(myClass);          
+          elems[i].classList.toggle(myClass);
+          let evt = createNewEvent("active");
+          elems[i].dispatchEvent(evt);
         }
         else {
           let arr = elems[i].className.split(" ");
@@ -41,7 +86,9 @@ function toggleClass(evt) {
           }
           else {
             arr.push(myClass);
-            elems[i].className = arr.join(" ");            
+            elems[i].className = arr.join(" ");
+            let evt = createNewEvent("active");
+            elems[i].dispatchEvent(evt);
           }
         }
       }
@@ -66,4 +113,17 @@ function toggleClass(evt) {
       elem.className = arr.join(" ");
     }
   }
+}
+
+function createNewEvent(evtName) {
+  let evt;
+  if (typeof Event === "function") {
+    evt = new Event(evtName);
+  }
+  else {
+    evt = document.createEvent("Event");
+    evt.initEvent(evtName, true, true);
+  }
+  
+  return evt;
 }
